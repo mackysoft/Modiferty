e71dc5aab78d7c60bb1d4a3840220c82eadfad2d
Makihiro <MackySoft@users.noreply.github.com>
2020-05-22 23:35:07 +0900

    Added extension methods for ModifierList

--- Assets/MackySoft/MackySoft.Modiferty/Runtime/ModifierListExtensions.cs
+++ Assets/MackySoft/MackySoft.Modiferty/Runtime/ModifierListExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MackySoft.Modiferty {
 	public static class ModifierListExtensions {
@@ -32,6 +34,38 @@ namespace MackySoft.Modiferty {
 		public static bool TryGetModifier<TModifier> (this IEnumerable<IModifier> source,out TModifier result) where TModifier : IModifier {
 			result = source.GetModifier<TModifier>();
 			return result != null;
+		}
+
+		/// <summary>
+		/// Remove all modifiers that match the specified type.
+		/// </summary>
+		/// <typeparam name="TModifier"> Type of Modifier to be removed. </typeparam>
+		public static int RemoveAll<TModifier> (this ICollection<IModifier> source) where TModifier : IModifier {
+			int removedModifiersCount = 0;
+			foreach (IModifier modifier in source.ToArray()) {
+				if (modifier is TModifier result) {
+					source.Remove(result);
+					removedModifiersCount++;
+				}
+			}
+			return removedModifiersCount;
+		}
+
+		/// <summary>
+		/// Remove all modifiers that match the condition.
+		/// </summary>
+		public static int RemoveAll (this ICollection<IModifier> source,Predicate<IModifier> match) {
+			if (match == null) {
+				throw new ArgumentNullException(nameof(match));
+			}
+			int removedModifiersCount = 0;
+			foreach (IModifier modifier in source.ToArray()) {
+				if (match.Invoke(modifier)) {
+					source.Remove(modifier);
+					removedModifiersCount++;
+				}
+			}
+			return removedModifiersCount;
 		}
 
 	}
