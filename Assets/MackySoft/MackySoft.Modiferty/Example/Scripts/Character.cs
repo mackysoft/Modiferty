c8b4d2315ee41c920b0ed355bff02fbbe3135d92
Makihiro <MackySoft@users.noreply.github.com>
2020-05-22 11:46:38 +0900

    Renamed assembly name
    Renamed assembly name "MackySoft.Modiferty" to "MackySoft.Modiferty.Runtime".

---
+++ Assets/MackySoft/MackySoft.Modiferty/Example/Scripts/Character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MackySoft.Modiferty.Example {
+	public class Character : MonoBehaviour {
+
+		public int health = 3;
+		public ModifiableInt attackPower = new ModifiableInt(2);
+
+		public void Attack (Character target) {
+			target.health -= attackPower.Evaluate();
+		}
+
+	}
+
+	public class CharacterWithoutModiferty : MonoBehaviour {
+
+		public int health = 3;
+		public int attackPower = 2;
+		public int additionalAttackPower = 0;
+
+		public void Attack (Character target) {
+			target.health -= attackPower + additionalAttackPower;
+		}
+	}
+
+	public class Character3 : MonoBehaviour {
+		public int health = 3;
+		public int attackPower = 2;
+		public List<float> attackPowerMultiply = new List<float>();
+
+		public void Attack (Character target) {
+			int result = attackPower;
+			foreach (float multiply in attackPowerMultiply) {
+
+			}
+			target.health -= attackPower + result;
+		}
+	}
+
+	public class PowerUpItemWithoutModiferty : MonoBehaviour {
+
+	}
+
+}
